cbfb0c887de51289a202264b0107461c239253b1
Nikhil Menon <nmenon1215@gmail.com>
2023-10-28 14:05:32 -0700

    Finalized Posn Interface

--- src/cs3500/model/Posn.java
+++ src/cs3500/model/Posn.java
@@ -1,8 +1,16 @@
 package cs3500.model;
 
+import java.util.List;
+
 /**
  * A Posn is a representation of a coordinate. Coordinates can be represented in many ways.
  * For example, they can be 3D coordinates with 3 nums, or 2D coordinates with 2 nums.
  */
 public interface Posn {
+  //OBSERVATIONS
+  /**
+   * Returns a list of coordinates for the posn. Order of coordinates is specified in the class
+   * @return the list of coordinates that correspond to this posn.
+   */
+  List<Integer> getCoords();
 }
