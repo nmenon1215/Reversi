df1e50d47902825534804c271ba506b7d6dff7c7
Nikhil Menon <nmenon1215@gmail.com>
2023-10-28 19:59:18 -0700

    added examples testing barebones

---
+++ test/cs3500/model/ReversiExamples.java
@@ -0,0 +1,13 @@
+package cs3500.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReversiExamples {
+
+  @Before
+  public void init() {}
+
+  @Test
+  public void testSomething() {}
+}
