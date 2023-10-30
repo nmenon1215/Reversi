0d1da0ec64f9cf10787dd31223b5965530e11b04
Simon Coleman <coleman.simon.e@gmail.com>
2023-10-30 16:42:21 -0400

    implementation for Tile and Posn

--- src/cs3500/model/HexagonalPosn.java
+++ src/cs3500/model/HexagonalPosn.java
@@ -1,15 +1,35 @@
 package cs3500.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Represents a cube coordinate system with q, r, and s being the directions.
+ * q is in the direction of northwest to southeast.
+ * r is in the direction of west to east.
+ * s is in the direction of southwest to northeast.
+ */
 public class HexagonalPosn implements Posn {
 
-  HexagonalPosn() {}
+  private final int q, r, s;
 
-  String errormsg = "If you got this to run, the code compiles!";
+  HexagonalPosn(int q, int r, int s) {
+    this.q = q;
+    this.r = r;
+    this.s = s;
+  }
 
+  /**
+   * We are returning the coordinates as (q, r, s).
+   * @return (q, r, s)
+   */
   @Override
   public List<Integer> getCoords() {
-    throw new RuntimeException(errormsg);
+    List<Integer> coord = new ArrayList<>();
+    coord.add(q);
+    coord.add(r);
+    coord.add(s);
+    return coord;
   }
 }
