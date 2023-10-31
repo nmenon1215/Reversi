5b7c7088626076d55c1d3a9ff6b4a22ab4a936cd
Nikhil Menon <nmenon1215@gmail.com>
2023-10-31 12:42:17 -0700

    Testing for Tile and Posn

--- test/cs3500/model/TestTileImplementation.java
+++ test/cs3500/model/TestTileImplementation.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class TestTileImplementation {
   ITile tile;
   @Before
@@ -29,19 +31,30 @@ public class TestTileImplementation {
   // TESTING getPlayer()
   @Test
   public void getPlayerReturnsNullWhenTileIsUnoccupied() {
+    Assert.assertEquals(null, tile.getPlayer());
   }
 
   @Test
   public void getPlayerReturnsCorrectPlayerWhenOccupied() {
+    tile.flipTo(new User('X'));
+    Assert.assertEquals("X", tile.getPlayer().toString());
   }
 
   // TESTING getPosn()
   @Test
   public void getPosnWorksOnTile() {
+    Assert.assertEquals(new HexagonalPosn(0, 0, 0), tile.getPosition());
   }
 
   // TESTING Posn class
   @Test
   public void getCoorsReturnsCoordinatesInCorrectOrder() {
+    Posn increasing = new HexagonalPosn(-3, 1, 2);
+    Assert.assertEquals(List.of(-3, 1, 2), increasing.getCoords());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void posnCoordinatesMustAddToZero() {
+    Posn invalid = new HexagonalPosn(1, 2, 3);
   }
 }
