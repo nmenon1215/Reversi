18a525a382b3854bcc5870b5c64ad9f9c2a3b425
Simon Coleman <coleman.simon.e@gmail.com>
2023-10-31 15:12:13 -0400

    added a single test

--- test/cs3500/model/TestModelImplementation.java
+++ test/cs3500/model/TestModelImplementation.java
@@ -80,4 +80,8 @@ public class TestModelImplementation {
   @Test
   public void getScoreOfPlayerReturnsCorrectValueMidGame() {
   }
+
+  @Test
+  public void compareAIAndUserWithSamePlayerValue() {
+  }
 }
