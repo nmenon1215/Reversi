6b2eb28f27ce74c22dd66b3e08c68b11e495c0ed
Nikhil Menon <nmenon1215@gmail.com>
2023-10-31 11:00:26 -0700

    flipTo method tests

--- test/cs3500/model/TestTileImplementation.java
+++ test/cs3500/model/TestTileImplementation.java
@@ -1,20 +1,29 @@
 package cs3500.model;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestTileImplementation {
+  ITile tile;
   @Before
   public void init() {
+    tile = new HexagonalTile(new HexagonalPosn(0,0,0));
   }
 
   // TESTING flipTo(Player p)
-  @Test
+  @Test(expected = IllegalArgumentException.class)
   public void flipToThrowsIllegalArgWhenPlayerIsNull() {
+    tile.flipTo(null);
   }
 
   @Test
   public void flipToWithAnyPlayerWorks() {
+    tile.flipTo(new User('A'));
+    Assert.assertEquals(new AI('A'), tile.getPlayer());
+
+    tile.flipTo(new AI('c'));
+    Assert.assertEquals(new User('c'), tile.getPlayer());
   }
 
   // TESTING getPlayer()
