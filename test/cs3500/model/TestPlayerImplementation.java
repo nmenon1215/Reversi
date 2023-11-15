2c92beaa17d21829785639b93a027d4235576135
Simon Coleman <coleman.simon.e@gmail.com>
2023-11-15 14:22:00 -0500

    tests for AI

--- test/cs3500/model/TestPlayerImplementation.java
+++ test/cs3500/model/TestPlayerImplementation.java
@@ -4,23 +4,62 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cs3500.player.AI;
+import cs3500.player.CaptureMaxPieces;
 import cs3500.player.Player;
 import cs3500.player.User;
 
 public class TestPlayerImplementation {
   Player p1;
-  Player ai;
+  Player aiBasic;
 
   @Before
   public void init() {
     p1 = new User('X');
-    ai = new AI('X');
+    aiBasic = new AI('X', new ArrayList<>(List.of(new CaptureMaxPieces())));
   }
 
   // TESTING equals(Object obj)
   @Test
   public void compareAIAndUserWithSamePlayerValue() {
-    Assert.assertEquals(p1, ai);
+    Assert.assertEquals(p1, aiBasic);
   }
+
+  // TESTING AI
+
+  // TESTING CaptureMaxPieces Strategy
+  @Test
+  public void choosesMoveWithMoreTilesToBeFlipped() {
+
+  }
+
+  @Test
+  public void ensureModelCannotBeNull() {}
+
+  @Test
+  public void ensurePlayerCannotBeNull() {}
+
+  @Test
+  public void ensureThereCannotBeNullMoves() {}
+
+  @Test
+  public void ensureHavingNoMovesThrowsAnException() {}
+
+  @Test
+  public void ensureGivenMovesAreActuallyValid() {}
+
+  // TESTING AvoidCellsNextToCorners Strategy
+
+  // TESTING PlaceAtCorners
+
+  // TESTING CaptureMaxPieces as Priority Strategy
+
+  // TESTING AvoidCellsNextToCorners as Priority Strategy
+
+  // TESTING PlaceAtCorners as Priority Strategy
+
+
 }
